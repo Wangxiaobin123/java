f8969747f8679c79e35fd013e02a770941e5c034
shengbin <shengbin.wang@baifendian.com>
2018-09-30 14:15:22 +0800

    disruptor tes

---
+++ disruptor_test/src/main/java/disruptorTest/MyInParkingDataEvent.java
@@ -0,0 +1,14 @@
+package disruptorTest;
+
+public class MyInParkingDataEvent {
+
+    private String carLicense; // 车牌号
+
+    public String getCarLicense() {
+        return carLicense;
+    }
+
+    public void setCarLicense(String carLicense) {
+        this.carLicense = carLicense;
+    }
+}
